cd8f450286204418b5cd975e0c7f4980d5fba001
TomBeachAAS <tom.beach@autonomousagrisolutions.co.uk>
2026-07-14 01:34:07 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,6 +29,7 @@ export async function middleware(request: NextRequest) {
     }
   );
 
+  
   const { data: { user } } = await supabase.auth.getUser();
 
   // Public routes that don't require auth
