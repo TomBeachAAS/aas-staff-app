260779b5683aa8cd0e7e235fd6bdf69442176546
TomBeachAAS <tom.beach@autonomousagrisolutions.co.uk>
2026-07-15 22:44:24 +0100

    Update page.tsx

--- src/app/(app)/equipment/[id]/page.tsx
+++ src/app/(app)/equipment/[id]/page.tsx
@@ -86,6 +86,7 @@ export default async function EquipmentDetailPage({ params }: { params: Promise<
             Drop pin
           </button>
           
+          <a
             id="dir-btn"
             href={dirHref}
             target="_blank"
