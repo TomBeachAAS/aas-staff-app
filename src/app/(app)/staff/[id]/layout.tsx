ef99fbb0671167d647b9bcb3d8c874a94d3f0038
TomBeachAAS <tom.beach@autonomousagrisolutions.co.uk>
2026-07-16 15:09:27 +0100

    Fix duplicate Permissions card: remove StaffPermissions from staff/[id]/layout

--- src/app/(app)/staff/[id]/layout.tsx
+++ src/app/(app)/staff/[id]/layout.tsx
@@ -1,10 +1,3 @@
-import { StaffPermissions } from '@/components/staff/StaffPermissions';
-
 export default function StaffDetailLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <StaffPermissions />
-    </>
-  );
+  return <>{children}</>;
 }
